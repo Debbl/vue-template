76f5669e584c401c61723fc8328cac0c66517910
Debbl <me@aiwan.run>
2022-09-29 21:38:40 +0800

    add vue-router

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,11 @@
 <script setup lang="ts"></script>
 
 <template>
-  <div class="text-red-900">app</div>
+  <div class="h-screen flex-col flex justify-center items-center">
+    <div>
+      <router-link to="/">home</router-link>
+      <router-link to="/about">about</router-link>
+    </div>
+    <router-view />
+  </div>
 </template>
