817fd97b8473082267fcaa0df3f078e74f98f458
Debbl <me@aiwan.run>
2022-09-29 22:05:21 +0800

    add dark mode

---
+++ src/components/Footer.vue
@@ -0,0 +1,10 @@
+<script lang="ts" setup>
+import { useDark } from "@vueuse/core";
+const isDark = useDark();
+</script>
+
+<template>
+  <div>
+    <button @click="isDark = !isDark">{{ isDark ? "黑" : "白" }}</button>
+  </div>
+</template>
